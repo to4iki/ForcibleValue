c58e172bfadb4d270ffb80be5881dc833b150cdd
Toshiki Takezawa <tsk.take815@gmail.com>
2024-11-26 00:15:12 +0900

    Bump up swift-tools-version to 6.0

--- Package.swift
+++ Package.swift
@@ -1,17 +1,9 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let debugSwiftSettings: [PackageDescription.SwiftSetting] = [
-  .enableUpcomingFeature("ConciseMagicFile", .when(configuration: .debug)), // SE-0274
-  .enableUpcomingFeature("ForwardTrailingClosures", .when(configuration: .debug)), // SE-0286
   .enableUpcomingFeature("ExistentialAny", .when(configuration: .debug)), // SE-0335
-  .enableUpcomingFeature("BareSlashRegexLiterals", .when(configuration: .debug)), // SE-0354
-  .enableUpcomingFeature("DeprecateApplicationMain", .when(configuration: .debug)), // SE-0383
-  .enableUpcomingFeature("ImportObjcForwardDeclarations", .when(configuration: .debug)), // SE-0384
-  .enableUpcomingFeature("DisableOutwardActorInference", .when(configuration: .debug)), // SE-0401
-  .enableUpcomingFeature("IsolatedDefaultValues", .when(configuration: .debug)), // SE-0411
-  .enableUpcomingFeature("GlobalConcurrency", .when(configuration: .debug)), // SE-0412
 ]
 
 let package = Package(
@@ -19,18 +11,20 @@ let package = Package(
   products: [
     .library(
       name: "ForcibleValue",
-      targets: ["ForcibleValue"])
+      targets: ["ForcibleValue"]
+    )
   ],
   dependencies: [],
   targets: [
     .target(
       name: "ForcibleValue",
-      dependencies: []),
+      dependencies: []
+    ),
     .testTarget(
       name: "ForcibleValueTests",
-      dependencies: ["ForcibleValue"]),
-  ],
-  swiftLanguageVersions: [.v5]
+      dependencies: ["ForcibleValue"]
+    ),
+  ]
 )
 
 for target in package.targets {
